2e5f0f48a42caee5f12a42c64cdfaaacaad6f24a
Daniela Baptista <m.danielabap@gmail.com>
2024-03-13 22:45:48 +0100

    highest death rate per Country added

--- ~vsE716.sql
+++ ~vsE716.sql
@@ -1,6 +1,6 @@
 SELECT *
 FROM Project..CovidDeaths
-ORDER BY 3,4;
+ORDER BY 3,4
 
 --SELECT *
 --FROM Project..CovidVaccinations
@@ -9,9 +9,10 @@ ORDER BY 3,4;
 --DELETE FROM Project..CovidVaccinations
 --WHERE continent is null
 
-SELECT location, date, total_cases, new_cases, total_deaths, population
+SELECT location, date, CONVERT(float,total_cases) AS Total_cases, new_cases, CONVERT(float,total_deaths) as Total_deaths, population
 FROM Project..CovidDeaths
-ORDER BY 1,2;
+WHERE location like '%states%'
+ORDER BY 5 desc
 
 
 -- Total Cases vs Total Deaths
@@ -19,14 +20,14 @@ ORDER BY 1,2;
 SELECT location, date, total_cases, total_deaths, (CONVERT(float,total_deaths)/CONVERT(float,total_cases))*100 AS DeathPercentage
 FROM Project..CovidDeaths
 WHERE location like '%states%'
-ORDER BY 1,2;
+ORDER BY 1,2
 
 -- Total Cases vs Population
 -- Percentage of Deaths
 SELECT location, date, population, total_cases, (CONVERT(float,total_cases)/population)*100 AS CasesPercentage
 FROM Project..CovidDeaths
 WHERE location like '%states%'
-ORDER BY 1,2;
+ORDER BY 1,2
 
 -- Highest Infection Rate compared to Population
 SELECT location, population, MAX(CONVERT(float,total_cases)) AS HighestInfectionCountry, MAX((CONVERT(float,total_cases)/population))*100 
@@ -37,4 +38,8 @@ GROUP BY location, population
 ORDER BY PercentPopulationInfected DESC
 
 
-SELECT
+-- Highest Death Rate per Population
+SELECT location, MAX(CONVERT(float,total_deaths)) AS TotalDeathCountry
+FROM Project..CovidDeaths
+GROUP BY location
+ORDER BY TotalDeathCountry DESC
