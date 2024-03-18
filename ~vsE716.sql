51b02c5a9cf13b5490e400eea1aea3a9115f6f2e
Daniela Baptista <m.danielabap@gmail.com>
2024-03-18 12:14:16 +0100

    total deaths by country

--- ~vsE716.sql
+++ ~vsE716.sql
@@ -1,6 +1,13 @@
 SELECT *
 FROM Project..CovidDeaths
+--WHERE total_cases is not null
 ORDER BY 3,4
+
+SELECT location, date, new_vaccinations, total_vaccinations
+FROM Project..CovidVaccinations
+--WHERE total_cases is not null
+WHERE location like 'Venezuela'
+ORDER BY 1,2 
 
 --SELECT *
 --FROM Project..CovidVaccinations
@@ -43,3 +50,28 @@ SELECT location, MAX(CONVERT(float,total_deaths)) AS TotalDeathCountry
 FROM Project..CovidDeaths
 GROUP BY location
 ORDER BY TotalDeathCountry DESC
+
+-- let's take a look now by continent
+SELECT continent, MAX(CONVERT(float,total_deaths)) AS TotalDeathContinent
+FROM Project..CovidDeaths
+GROUP BY continent
+ORDER BY TotalDeathContinent DESC
+
+---- global numbers
+--SELECT date,SUM(new_cases) AS TotalNewCases
+--FROM Project..CovidDeaths 
+--GROUP BY date
+--ORDER BY 1
+
+SELECT dea.continent, dea.location, dea.date, population, vac.new_vaccinations, 
+	SUM(CONVERT(bigint, ISNULL(vac.new_vaccinations, 0))) OVER (PARTITION BY dea.location ORDER BY dea.location)
+	AS TotalVaccByCountry
+FROM Project..CovidDeaths dea
+JOIN Project..CovidVaccinations vac
+	ON dea.location = vac.location
+	AND dea.date = vac.date
+ORDER BY 2,3
+
+
+
+
